4f5c25d5bbec9175fbf5ce603bef2013149769d1
pedLJ <pedrigallarajane99@gmail.com>
2022-08-09 16:46:42 +0800

    Revert "Revert "Revert "Revert "Borrower's account functional(edit profile, change password)""""
    This reverts commit 87df9e8c3477bbd81c411ffd31b16336671df7c4.

--- Borrower/home.php
+++ Borrower/home.php
@@ -14,6 +14,21 @@
 	
 		<div class="content">
 			<div class="container">
+				<?php if($verified == 'Not Verified!'): ?>
+					<div class="alert alert-danger alert-dismissible text-center mt-0 m-0">
+						<button class="close" type="button" data-dismiss="alert">&times;</button>
+						Your email is not verified! We have sent a verification link on your email, please check!
+					</div>
+				<?php endif; ?>
+				<div class="card mt-4" style="width: 18rem;">
+					<a href="#" data-toggle="tooltip" data-placement="top" title="Short description here">
+						<img class="card-img-top" src="../stylesheets/images/logo.png" alt="Card image cap">
+					</a>
+					<div class="card-body">
+						<p class="card-title">Title here</p>
+						<p class="card-text text-muted text-sm">by: Author here</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
